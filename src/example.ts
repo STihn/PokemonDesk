4cffa0fc933cdc14a4ba194e1b9806bc61304ceb
STihn <you.lucky@bk.ru>
2021-05-13 22:59:23 +0300

    homework2/typescript

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,9 @@
 
-// let tsString: string = 'str';
-const tsString = 's';
+interface myHometask {
+    howIDoIt: string,
+	simeArray: ["string", "string", number],
+	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
 
 function concatStr(strOne: string, strTwo: string): void {
     const result = strOne + strTwo;
